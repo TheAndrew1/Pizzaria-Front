faea79d1f36e69a4d8583246132f6fc5ce668433
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-10-23 22:52:34 -0300

    link de adicionar e editar ajustado na lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SaborListComponent } from './sabor/sabor-list/sabor-list.component';
+import { SaborDetailsComponent } from './sabor/sabor-details/sabor-details.component';
 
 const routes: Routes = [
   {path:"", redirectTo: "sabores", pathMatch: "full"},
-  {path: "sabores", component: SaborListComponent}
+  {path: "sabores", component: SaborListComponent},
+  {path: "sabores/form", component: SaborDetailsComponent}
 ];
 
 @NgModule({
