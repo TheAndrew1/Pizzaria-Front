ad0d4909696c5b45162343a474f17e45f97721d7
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-10-25 21:31:39 -0300

    header e rotas implementadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SaborListComponent } from './components/sabor/sabor-list/sabor-list.component';
 import { SaborDetailsComponent } from './components/sabor/sabor-details/sabor-details.component';
+import { ProdutoListComponent } from './components/produto/produto-list/produto-list.component';
+import { ProdutoDetailsComponent } from './components/produto/produto-details/produto-details.component';
+import { IndexComponent } from './components/layout/index/index.component';
 
 const routes: Routes = [
-  {path:"", redirectTo: "sabores", pathMatch: "full"},
+  {path:"", redirectTo: "index", pathMatch: "full"},
+  {path: "index", component: IndexComponent},
   {path: "sabores", component: SaborListComponent},
-  {path: "sabores/form", component: SaborDetailsComponent}
+  {path: "sabores/form", component: SaborDetailsComponent},
+  {path: "produtos", component: ProdutoListComponent},
+  {path: "produtos/form", component: ProdutoDetailsComponent}
 ];
 
 @NgModule({
