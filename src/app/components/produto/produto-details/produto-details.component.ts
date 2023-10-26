8492878c11e429982f6e8ac583e02d77a9bb7b55
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-10-25 22:15:43 -0300

    crud produtos (exceto pizza)

--- src/app/components/produto/produto-details/produto-details.component.ts
+++ src/app/components/produto/produto-details/produto-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Produto } from 'src/app/models/produtoModel';
+import { ProdutoService } from 'src/app/services/produto/produto.service';
 
 @Component({
   selector: 'app-produto-details',
@@ -6,5 +9,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./produto-details.component.scss']
 })
 export class ProdutoDetailsComponent {
+  router = inject(Router);
+  rota = inject(ActivatedRoute);
+  produtoService = inject(ProdutoService);
 
+  produto: Produto = new Produto();
+  tipo!: string | null;
+  id!: number;
+  
+  constructor(){
+    this.rota.queryParamMap.subscribe(params => {
+      this.tipo = params.get("type");
+      this.id = Number(params.get("id"));
+    });
+
+    if(this.id != 0){
+      this.findById(this.id);
+    }
+  }
+
+  findById(id: number){
+    this.produtoService.findById(id).then(promise => promise.subscribe({
+      next: response => this.produto = response,
+      error: erro => console.log(erro)
+    }));
+  }
+
+  async salvar(){
+    if(this.tipo == "new"){
+       await this.produtoService.create(this.produto).then(promise => promise.subscribe({
+        next: response => this.produto = response,
+        error: erro => console.log(erro)
+      }))
+      .then(() => {
+        this.router.navigate(["/produtos"]);
+      })
+    }else{
+      await this.produtoService.edit(this.id, this.produto).then(promise => promise.subscribe({
+        next: response => this.produto = response,
+        error: erro => console.log(erro)
+      }))
+      .then(() => {
+        this.router.navigate(["/produtos"]);
+      })
+    }
+  }
 }
--- src/app/components/produto/produto-list/produto-list.component.ts
+++ src/app/components/produto/produto-list/produto-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Produto } from 'src/app/models/produtoModel';
+import { ProdutoService } from 'src/app/services/produto/produto.service';
 
 @Component({
   selector: 'app-produto-list',
@@ -6,5 +10,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./produto-list.component.scss']
 })
 export class ProdutoListComponent {
+  control = new FormControl('', { nonNullable: true });
+  modalService = inject(NgbModal);
 
+  produtoService = inject(ProdutoService);
+
+  produtos: Array<Produto> = new Array<Produto>();
+  produtos$?: Array<Produto>;
+
+  produtoSelecionado!: Produto;
+
+  constructor() {
+    this.findAll();
+	}
+
+  async findAll(){
+    await this.produtoService.findAll().then(promise => promise.subscribe({
+      next: response => {this.produtos = this.filterProducts(response);
+                        this.produtos$ = this.produtos},
+      error: erro => console.log(erro)
+    }));
+  }
+
+  async delete(id: number){
+    await this.produtoService.delete(id)
+      .then(promise => promise.subscribe({
+        next: response => {
+          this.findAll();
+        },
+        error: erro => console.log(erro)
+      }))
+      .then(() => {
+        this.modalService.dismissAll();
+      })
+  }
+
+  filter(){
+    if(this.control.value == ""){
+      this.produtos$ = this.produtos
+    }else{
+      this.produtos$ = this.produtos.filter(produto => produto.nome.includes(this.control.value));
+    }
+  }
+
+  filterProducts(produtos: Produto[]): Produto[]{
+    return produtos.filter(produto => produto.tamanho == null);
+  }
+
+  openModal(content: any, produto: Produto) {
+    this.produtoSelecionado = produto;
+
+		this.modalService.open(content, { centered: true });
+	}
 }
