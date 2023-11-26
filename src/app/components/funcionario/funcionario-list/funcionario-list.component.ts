dc1b202edd79c69c51cc7cda5f3f01b1f34e5428
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-11-26 09:39:17 -0300

    lista de funcionarios funcionando

--- src/app/components/funcionario/funcionario-list/funcionario-list.component.ts
+++ src/app/components/funcionario/funcionario-list/funcionario-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Funcionario } from 'src/app/models/funcionarioModel';
+import { FuncionarioService } from 'src/app/services/funcionario/funcionario.service';
 
 @Component({
   selector: 'app-funcionario-list',
@@ -6,5 +10,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./funcionario-list.component.scss']
 })
 export class FuncionarioListComponent {
+  control = new FormControl('', { nonNullable: true });
+  modalService = inject(NgbModal);
 
+  funcionarioService = inject(FuncionarioService);
+
+  funcionarios: Array<Funcionario> = new Array<Funcionario>();
+  funcionarios$?: Array<Funcionario>;
+
+  funcionarioSelecionado!: Funcionario;
+
+  constructor() {
+    this.findAll();
+	}
+
+  findAll(){
+    this.funcionarioService.findAll().subscribe({
+      next: response => {this.funcionarios = response;
+                        this.funcionarios$ = this.funcionarios},
+      error: erro => console.log(erro)
+    });
+  }
+
+  delete(id: number){
+    this.funcionarioService.delete(id)
+      .subscribe({
+        next: response => {
+          this.findAll();
+        },
+        error: erro => console.log(erro)
+      })
+
+    this.modalService.dismissAll();
+  }
+
+  filter(){
+    if(this.control.value == ""){
+      this.funcionarios$ = this.funcionarios
+    }else{
+      this.funcionarios$ = this.funcionarios.filter(sabor => sabor.nome.includes(this.control.value));
+    }
+  }
+
+  openModal(content: any, sabor: Funcionario) {
+    this.funcionarioSelecionado = sabor;
+
+		this.modalService.open(content, { centered: true });
+	}
 }
