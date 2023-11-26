457e0a696fdb8c1c5662122250c72f115e519bd4
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-11-26 10:41:17 -0300

    cadastro de clientes

--- src/app/components/cliente/cliente-details/cliente-details.component.ts
+++ src/app/components/cliente/cliente-details/cliente-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Cliente } from 'src/app/models/clienteModel';
+import { ClienteService } from 'src/app/services/cliente/cliente.service';
 
 @Component({
   selector: 'app-cliente-details',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./cliente-details.component.scss']
 })
 export class ClienteDetailsComponent {
+  router = inject(Router);
+  rota = inject(ActivatedRoute);
+  clienteService = inject(ClienteService);
 
+  cliente: Cliente = new Cliente();
+  tipo!: string | null;
+  id!: number;
+  showPassword: boolean = false;
+  
+  constructor(){
+    this.rota.queryParamMap.subscribe(params => {
+      this.tipo = params.get("type");
+      this.id = Number(params.get("id"));
+    });
+
+    if(this.id != 0){
+      this.findById(this.id);
+    }
+  }
+
+  findById(id: number){
+    this.clienteService.findById(id).subscribe({
+      next: response => this.cliente = response,
+      error: erro => console.log(erro)
+    });
+  }
+
+  salvar(){
+    if(this.tipo == "new"){
+       this.clienteService.create(this.cliente).subscribe({
+        next: response => this.cliente = response,
+        error: erro => console.log(erro)
+      })
+    }else{
+      this.clienteService.edit(this.id, this.cliente).subscribe({
+        next: response => this.cliente = response,
+        error: erro => console.log(erro)
+      })
+    }
+
+    this.router.navigate(["/clientes"]);
+  }
 }
