140bd85b142e41411116e5d4ecd9a997355120da
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-11-26 15:37:41 -0300

    cadastrando endereco

--- src/app/components/endereco/endereco-details/endereco-details.component.ts
+++ src/app/components/endereco/endereco-details/endereco-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Endereco } from 'src/app/models/enderecoModel';
+import { EnderecoService } from 'src/app/services/endereco/endereco.service';
 
 @Component({
   selector: 'app-endereco-details',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./endereco-details.component.scss']
 })
 export class EnderecoDetailsComponent {
+  router = inject(Router);
+  rota = inject(ActivatedRoute);
+  enderecoService = inject(EnderecoService);
 
+  endereco: Endereco = new Endereco();
+  tipo!: string | null;
+  id!: number;
+  showPassword: boolean = false;
+  
+  constructor(){
+    this.rota.queryParamMap.subscribe(params => {
+      this.tipo = params.get("type");
+      this.id = Number(params.get("id"));
+    });
+
+    if(this.id != 0){
+      this.findById(this.id);
+    }
+  }
+
+  findById(id: number){
+    this.enderecoService.findById(id).subscribe({
+      next: response => this.endereco = response,
+      error: erro => console.log(erro)
+    });
+  }
+
+  salvar(){
+    if(this.tipo == "new"){
+       this.enderecoService.create(this.endereco).subscribe({
+        next: response => this.endereco = response,
+        error: erro => console.log(erro)
+      })
+    }else{
+      this.enderecoService.edit(this.id, this.endereco).subscribe({
+        next: response => this.endereco = response,
+        error: erro => console.log(erro)
+      })
+    }
+
+    this.router.navigate(["/enderecos"]);
+  }
 }
