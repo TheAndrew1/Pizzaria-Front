30bb2b7c327e7ea8fb58c8483bc92f658c6b3415
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-11-26 23:13:21 -0300

    adicionando cliente no pedido

--- src/app/components/pedido/pedido-details/pedido-details.component.ts
+++ src/app/components/pedido/pedido-details/pedido-details.component.ts
@@ -1,6 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Cliente } from 'src/app/models/clienteModel';
+import { Endereco } from 'src/app/models/enderecoModel';
 import { Pedido } from 'src/app/models/pedidoModel';
+import { ClienteService } from 'src/app/services/cliente/cliente.service';
+import { EnderecoService } from 'src/app/services/endereco/endereco.service';
 import { PedidoService } from 'src/app/services/pedido/pedido.service';
 
 @Component({
@@ -9,13 +14,20 @@ import { PedidoService } from 'src/app/services/pedido/pedido.service';
   styleUrls: ['./pedido-details.component.scss']
 })
 export class PedidoDetailsComponent {
+[x: string]: any;
   router = inject(Router);
   rota = inject(ActivatedRoute);
+  modalService = inject(NgbModal);
+  
   pedidoService = inject(PedidoService);
+  enderecoService = inject(EnderecoService);
+  clienteService = inject(ClienteService);
 
   pedido: Pedido = new Pedido();
+  clientes!: Cliente[];
   tipo!: string | null;
   id!: number;
+  clienteId!: number;
   
   constructor(){
     this.rota.queryParamMap.subscribe(params => {
@@ -26,6 +38,11 @@ export class PedidoDetailsComponent {
     if(this.id != 0){
       this.findById(this.id);
     }
+
+    this.clienteService.findAll().subscribe({
+      next: clientes => this.clientes = clientes,
+      error: erro => console.log(erro)
+    })
   }
 
   findById(id: number){
@@ -36,6 +53,10 @@ export class PedidoDetailsComponent {
   }
 
   salvar(){
+    if(this.clienteId){
+      this.pedido.cliente = this.clientes.filter(cliente => cliente.id == this.clienteId)[0];
+    }
+
     if(this.tipo == "new"){
         this.pedidoService.create(this.pedido).subscribe({
         next: response => this.pedido = response,
@@ -49,5 +70,24 @@ export class PedidoDetailsComponent {
     }
 
     this.router.navigate(["/pedidos"]);
+  }
+
+  removeEndereco(id: number){
+    this.pedido.endereco = new Endereco();
+    this.enderecoService.delete(id-1)
+      .subscribe({
+        next: response => {},
+        error: erro => console.log(erro)
+      })
+
+    this.modalService.dismissAll();
+  }
+
+  openModal(content: any) {
+		this.modalService.open(content, { centered: true, size: "md" });
+	}
+
+  addEndereco(endereco: Endereco){
+    this.pedido.endereco = endereco;
   }
 }
